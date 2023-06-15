ad099667accd75b910c0abeeb8ba527721f7e9be
Bram Kelchtermans <bramkel@gmail.com>
2023-06-15 22:52:38 +0200

    Get system info history

--- backend/src/controllers/system.controller.js
+++ backend/src/controllers/system.controller.js
@@ -3,6 +3,8 @@ const pick = require('../utils/pick');
 const ApiError = require('../utils/ApiError');
 const catchAsync = require('../utils/catchAsync');
 const { systemService } = require('../services');
+const { Op } = require('sequelize');
+const SystemInformation = require('../database/models').SystemInformation;
 
 const getInfo = catchAsync(async (req, res) => {
     const result = await systemService.getSystemInfo();
@@ -14,7 +16,22 @@ const getCPUInfo = catchAsync(async (req, res) => {
     res.json(result);
 });
 
+const getSystemInfoHistory = catchAsync(async (req, res) => {
+    console.log("Here we go")
+    const moment = require('moment');
+    const result = await SystemInformation.findAndCountAll({
+        where: {
+            createdAt: {
+                [Op.gte]: moment().subtract(2, 'hours').toDate()
+            }
+        }
+    })
+
+    res.json(result);
+});
+
 module.exports = {
     getInfo,
     getCPUInfo,
+    getSystemInfoHistory,
 } 
--- backend/src/routes/system.route.js
+++ backend/src/routes/system.route.js
@@ -2,12 +2,30 @@ const express = require('express');
 const systemController = require('../controllers/system.controller')
 const router = express.Router();
 
+const HistoryRoutes = () => {
+    const _router = express.Router();
+
+    _router
+        .route('/')
+        .get(systemController.getSystemInfoHistory);
+    
+    return _router;
+}
+
+
 router
     .route('/')
     .get(systemController.getInfo);
 
 router
-    .route('/disks')
+    .route('/cpu')
     .get(systemController.getCPUInfo);
+
+router
+    .route('/history')
+    .get(systemController.getSystemInfoHistory);
+
+// router.use('/history', HistoryRoutes);
+
 
 module.exports = router;
