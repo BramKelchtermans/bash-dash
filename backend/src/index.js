9582fe68f343c5875c1c3963e30b560e41a44186
Bram Kelchtermans <bramkel@gmail.com>
2023-06-15 22:18:47 +0200

    Add support for jobs

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,13 @@
 const app = require('./app');
 const config = require('./config/config');
 const logger = require('./config/logger');
+const { Schedule } = require('./jobs/schedule');
 
 let server = app.listen(config.port, () => {
   logger.info(`Listening to port ${config.port}`);
 });
+
+Schedule.run();
 
 const exitHandler = () => {
   if (server) {
