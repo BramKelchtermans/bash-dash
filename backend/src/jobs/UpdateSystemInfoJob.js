ac9f66bab8b5a7fb5c3e38b63b1d3260ec6bc048
Bram Kelchtermans <bramkel@gmail.com>
2023-06-18 21:05:30 +0200

    Major backend refactor

--- backend/src/jobs/UpdateSystemInfoJob.js
+++ backend/src/jobs/UpdateSystemInfoJob.js
@@ -1,23 +1,21 @@
-const { systemService } = require("../services");
-const { Job } = require("./job");
-const SystemInformation = require("../models").SystemInformation;
+import SystemInfo from "../models/SystemInfo";
+import SystemService from "../services/SystemService";
+import { Job } from "./job";
+
 
 class UpdateSystemInfo extends Job {
     async fire() {
-        const raw = await systemService.getSystemInfo();
+        const raw = await SystemService.getSystemInfo();
         try {
             const data = {};
             for (let key in raw) {
                 data[key] = JSON.stringify(raw[key]);
             }
-            const entry = SystemInformation.build(data)
+            const entry = SystemInfo.build(data)
             await entry.save();
         } catch (e) {
             console.log(e)
         }
     }
 }
-
-module.exports = {
-    UpdateSystemInfo
-}
+export default UpdateSystemInfo;
